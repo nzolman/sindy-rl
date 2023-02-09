b76d53cfe001da69d61ab859d93e51c2cc1ffffd
Nick Zolman <nzolman@uw.edu>
2023-02-08 22:46:45 -0800

    added try/except for hydrogym import

--- sindy_rl/envs/__init__.py
+++ sindy_rl/envs/__init__.py
@@ -1,3 +1,8 @@
 from .inverted_pendulum import InvPendSurrogate
 from .swimmer import SwimmerSurrogate
-from .hydroenv import SurrogateCylinder
+import warnings
+
+try:
+    from .hydroenv import SurrogateCylinder
+except ImportError:
+    warnings.warn("Hydrogym not found! Can't use hydrogym environments")
