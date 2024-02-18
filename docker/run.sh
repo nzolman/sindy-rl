9f7ec2c009bacbea3166e31b5db1710715a2658a
Nick Zolman <nzolman@uw.edu>
2024-02-18 07:43:18 -0800

    minor updates to docker

--- docker/run.sh
+++ docker/run.sh
@@ -1,1 +1,6 @@
-docker run -u 0 -v /home/nzolman/projects/sindy-rl:/home/firedrake/sindy-rl:z -p 8888:8888 -it hydrogym-sindy:latest /bin/bash
+docker run -u 0 \
+            -v /home/nzolman/projects/sindy-rl:/home/firedrake/sindy-rl:z \
+            -v /local/nzolman:/local/nzolman:z \
+            -p 8888:8888 \
+            -it hydrogym-sindy:latest \
+            /bin/bash
