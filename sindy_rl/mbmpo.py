f85003816c1319eb216c24eea68471559386f709
Nick Zolman <nzolman@uw.edu>
2024-03-07 11:07:23 -0800

    added mbmpo comments

--- sindy_rl/mbmpo.py
+++ sindy_rl/mbmpo.py
@@ -1,21 +1,25 @@
+'''
+    Used in Benchmarking the swingup environment.
+    Section 5.1 of arXiv paper.
+'''
+
 import os
 import numpy as np
 import gymnasium as gym
-from gymnasium.envs.classic_control import PendulumEnv
 
 from ray.rllib.algorithms.mbmpo import MBMPOConfig, MBMPO
 from ray.rllib.algorithms.mbmpo.model_ensemble import DynamicsEnsembleCustomModel
-from ray.tune.registry import register_env
 from ray import tune, air
 
 from sindy_rl import _parent_dir
 from sindy_rl.registry import  DMCEnvWrapper
 from sindy_rl.reward_fns import cart_reward
 
-_MAX_EP_STEPS = 300
+_MAX_EP_STEPS = 1000 # steps to rollout per episode (before reset)
 
 class MBMPOCart(DMCEnvWrapper):
-    '''A wrapper to make this compatibile with RLlib's
+    '''
+    An env wrapper to make this compatibile with RLlib's
     MBMPO compatability.
     '''
     
@@ -35,45 +39,30 @@ class MBMPOCart(DMCEnvWrapper):
             'frame_skip': 1,
             'from_pixels': False,
             'task_kwargs':{
-                'time_limit': 3 # DEFAULT IS 10!
+                'time_limit': 10 # number of seconds to rollout per episode (dt = 0.01)
             }
         }
         
         default_cart_config.update(config)
         env_config = default_cart_config
         
-        # if env_config.get('use_bounds', None):
-        #     self.use_bounds = env_config.pop('use_bounds')
-        # else:
-        #     self.use_bounds = False
 
         super().__init__(env_config)
 
     def reward(self, obs, action, obs_next):
+        '''
+            MBMPO explicitly needs environments to have a `reward` function.
+            This is the lazy approach for wrapping existing code. 
+        '''
         costs = np.array([cart_reward(x, u) for x, u in zip(obs_next, action)])
         return costs
-    
-    # Can't actually provide callbacks or boundaries
-    # inside the dynamics updates!
-    # def step(self, action):
-    #     '''Incorporating extra callbacks and reset conditions'''
-    #     obs, rew, term, trun, info = super().step(action)
-        
-    #     obs = project_cartpole(obs)
-    #     term = term or self.is_term(obs)
-        
-    #     return obs, rew, term, trun, info 
-
-    # def is_term(self, obs):
-    #     term = False
-    #     if self.use_bounds:
-    #         lower_bounds_done = np.any(obs <= self.obs_bounds[0])
-    #         upper_bounds_done = np.any(obs >= self.obs_bounds[1])
-    #         term = lower_bounds_done or upper_bounds_done
-    #     return term
-    
 
 
+# ---------------------------------
+# Setup RLlib Training
+# ---------------------------------
+
+# MBMPO dynamics config
 dynamics_model = {
             "custom_model": DynamicsEnsembleCustomModel,
             # Number of Transition-Dynamics (TD) models in the ensemble.
@@ -93,9 +82,10 @@ dynamics_model = {
         }
 
 config = MBMPOConfig()
+
+# evaluation for mbmpo seems to be broken in rllib==2.6.3? 
 config = (config.training(lr=0.0003, dynamics_model=dynamics_model)
                 .environment(env=MBMPOCart)
-                # .environment(env_config=env_config)
                 .rollouts(num_rollout_workers=1)
                 .resources(num_cpus_per_worker=3)
                 # .evaluation(
@@ -114,23 +104,18 @@ fcnet_hiddens = [32, 32]
 model_config.update({'fcnet_hiddens': fcnet_hiddens})
 config.training(model=model_config)
 
-# algo = config.build(env=MBMPOCart)
-# res = algo.train()
-
 LOCAL_DIR =  os.path.join(_parent_dir, 'ray_results', 'mbmpo')
 
 run_config=air.RunConfig(
     local_dir=LOCAL_DIR,
-    name='dmc-cart-test_dyn-nn=512_policy-nn=32_steps=300',
+    name='dmc-cart-test_dyn-nn=512_policy-nn=32_steps=1k',
     stop={'training_iteration': 5000},
     log_to_file=True,
     checkpoint_config=air.CheckpointConfig(
         checkpoint_frequency=10),
 )
 
-tune_config=tune.TuneConfig(num_samples=20,
-                            # scheduler=pbt_sched,
-                            )
+tune_config=tune.TuneConfig(num_samples=20)
 
 tuner = tune.Tuner(
     MBMPO,
@@ -138,4 +123,6 @@ tuner = tune.Tuner(
     run_config=run_config,
     tune_config=tune_config,
 )
+
+# launch experiment
 results = tuner.fit()
