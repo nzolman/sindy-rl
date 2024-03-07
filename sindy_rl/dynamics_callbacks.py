2440bb612dce804fda256229867feaeb4ccd68e9
Nick Zolman <nzolman@uw.edu>
2024-03-07 12:05:24 -0800

    new comments

--- sindy_rl/dynamics_callbacks.py
+++ sindy_rl/dynamics_callbacks.py
@@ -1,15 +1,24 @@
+# --------------------------------------------------------
+# Intended to be used during dynamics models predict(x, u)
+# --------------------------------------------------------
+
 import numpy as np
 
 def project_cartpole(z):
+    '''Projecting the dm_control swingup task back onto the circle'''
     cart_pos, cos_th, sin_th, dx, dth = z
     
     u = np.array([cos_th, sin_th])
     new_cos, new_sin = u/np.linalg.norm(u)
     return np.array([cart_pos, new_cos, new_sin, dx, dth])
 
+
 # only works for (d,)-shaped vectors
 def project_cartpole_n(z, n=2): 
     '''
+    USE WITH CAUTION.
+    n-length pendulum observation project.
+    
     Cart Obs appears to be of the form:
         [x, cos(t_1), sin(t_1), ..., cos(t_k), sin(t_k)]
         concatenated with derivatives:
