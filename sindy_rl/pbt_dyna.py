5f700dcd22036130ece18f968fdcd45cc1becc4b
Nick Zolman <nzolman@uw.edu>
2024-03-08 11:19:17 -0800

    commented pbt runs

--- sindy_rl/pbt_dyna.py
+++ sindy_rl/pbt_dyna.py
@@ -1,46 +1,40 @@
 import warnings
 warnings.filterwarnings('ignore')
-
 import logging
 import os
 import numpy as np
+
 from ray.rllib.algorithms.registry import ALGORITHMS as rllib_algos
 from ray.tune.schedulers import PopulationBasedTraining
 from ray import tune, air
 from ray.air import session, Checkpoint
-# from ray import tune, train
-# from ray.train import Checkpoint
-from ray.rllib.algorithms.algorithm import Algorithm
 
 from sindy_rl.dyna import DynaSINDy
-from sindy_rl.env import rollout_env
-
-import tempfile
 
 
 def dyna_sindy(config):
+    '''
+    ray.Tune functional API for defining an expirement
+    '''
     dyna_config = config
     train_iterations = config['n_train_iter']
     dyn_fit_freq = config['dyn_fit_freq']
     ckpt_freq = config['ray_config']['checkpoint_freq']
     
     # data collected (or loaded) upon initialization
-    # TO-DO: this should depend on whether there's a checkpoint, probably. 
     dyna = DynaSINDy(dyna_config)
     
     start_iter = 0
     
+    # for PBT, session is populated with a checkpoint after evaluating the population
+    # and pruning the bottom performers
     checkpoint = session.get_checkpoint()
     if checkpoint:
-        
         check_dict = checkpoint.to_dict()
-        checkpoint_dir = check_dict['checkpoint_dir']
-        algo_dir = check_dict['algo_dir']
         dyna.load_checkpoint(check_dict)
         
-        # grab the iteration to make sure we _probably_ are checkpointing correctly again...? 
+        # grab the iteration to make sure we are checkpointing correctly
         start_iter = check_dict['epoch'] + 1
-        # print('\n!!! LOADING !!!', start_iter, session.get_trial_id(), checkpoint_dir, algo_dir)
 
         
     # setup the dynamics, reward, DRL algo push weights to surrogate
@@ -48,37 +42,16 @@ def dyna_sindy(config):
     dyna.fit_dynamics()
     dyna.fit_rew()
     dyna.update_surrogate()
-    
-    
-    # # !!!EXPERIMENTAL!!!
-    # eval_freq = dyna_config['drl']['config']['evaluation']['evaluation_interval']
-    # num_eval = dyna_config['drl']['config']['evaluation']['evaluation_duration']
 
-    
-    # custom_eval_dict = {'mean_rew': np.nan,
-    #                     'mean_len': 0}
-
-    # TO-DO: figure out the proper way to deal with this
     collect_dict = {'mean_rew': np.nan, 
                     'mean_len': 0}
     
+    # Main training loop
     for n_iter in range(start_iter, train_iterations):
-        # print('\n!!!n_iter!!', n_iter, session.get_trial_id())
         checkpoint = None
         train_results = dyna.train_algo()
-        
-        
-        # # !!!EXPERIMENTAL!!!
-        # if n_iter % eval_freq == eval_freq - 1:
-        #     dyna.real_env.reset_on_bounds = False
-        #     eval_obs, eval_acts, eval_rews = rollout_env(dyna.real_env, dyna.on_policy_pi, n_steps = num_eval * 1000)
-            
-        #     custom_eval_dict['mean_rew'] = np.mean([rew.sum() for rew in eval_rews])
-        #     custom_eval_dict['mean_len'] = np.mean([len(rew) for rew in eval_rews])
-        #     dyna.real_env.reset_on_bounds = True
 
-
-        # periodically collect data
+        # periodically evaluate by collecting on-policy data
         if (n_iter % dyn_fit_freq) == dyn_fit_freq - 1:
             (trajs_obs, 
              trajs_acts, 
@@ -87,7 +60,6 @@ def dyna_sindy(config):
                                             dyna.on_policy_pi,
                                             **dyna_config['on_policy_buffer']['collect']
                                             )
-
             dyna.fit_dynamics()
             dyna.fit_rew()
             dyna.update_surrogate()
@@ -104,13 +76,12 @@ def dyna_sindy(config):
                                                 )
             checkpoint = Checkpoint.from_dict(check_dict)
         
-        
-        
-        # train_results['custom_eval'] = custom_eval_dict
+        # compile metrics for tune to report
         train_results['traj_buffer'] = dyna.get_buffer_metrics()
         train_results['dyn_collect'] = collect_dict
         
-        # TO-DO: determine if this is necessary.
+        # may not be entirely necessay, copied from some official Tune
+        #   pbt example
         if checkpoint:
             session.report(train_results, 
                            checkpoint=checkpoint)
@@ -139,29 +110,17 @@ if __name__ == '__main__':
     
     from sindy_rl.policy import RandomPolicy
     from sindy_rl import _parent_dir
-    filename = '/home/firedrake/sindy-rl/sindy_rl/refactor/config_templates/dyna_cylinder_baseline.yml'
-    # filename = '/home/firedrake/sindy-rl/sindy_rl/refactor/config_templates/dyna_cylinder_test.yml'
-    # filename = '/home/firedrake/sindy-rl/sindy_rl/refactor/config_templates/dyna_pinball/Re=120_lift_new_params.yml'    
-    # filename = '/home/firedrake/sindy-rl/sindy_rl/refactor/config_templates/dyna_pinball/Re=120_square_new_params.yml'
-    # filename = '/home/firedrake/sindy-rl/sindy_rl/refactor/config_templates/dyna_pinball/Re=100_lift_new_params.yml'
-    # filename = '/home/firedrake/sindy-rl/sindy_rl/refactor/config_templates/dyna_pinball/Re=100_square_new_params.yml'
-    # filename = '/home/firedrake/sindy-rl/sindy_rl/refactor/config_templates/dyna_pinball/Re=30_square_new_params.yml'
-    # filename = '/home/firedrake/sindy-rl/sindy_rl/refactor/config_templates/dyna_pinball/Re=30_lift_new_params.yml'
-    # filename = '/home/firedrake/sindy-rl/sindy_rl/refactor/config_templates/dyna_pinball_baseline_debug.yml'
-    # filename = '/home/firedrake/sindy-rl/sindy_rl/refactor/dyna_pinball_test.yml'
-    # filename = '/home/firedrake/sindy-rl/sindy_rl/refactor/dyna_pinball_baseline.yml'
-    # filename = '/home/firedrake/sindy-rl/sindy_rl/refactor/dyna_pinball_baseline_debug.yml'
-    # filename = '/home/firedrake/sindy-rl/sindy_rl/refactor/dyna_cylinder_baseline.yml'
-    # filename = '/home/firedrake/sindy-rl/sindy_rl/refactor/dyna_cylinder_test.yml'
-    # filename = '/home/nzolman/projects/sindy-rl/sindy_rl/refactor/dyna_config_cart_nn_test.yml'
-    # filename = '/home/nzolman/projects/sindy-rl/sindy_rl/refactor/dyna_config_cart_test.yml'
-    # filename = '/home/nzolman/projects/sindy-rl/sindy_rl/refactor/dyna_config_cart_lin_test.yml'
-    # filename = '/home/nzolman/projects/sindy-rl/sindy_rl/refactor/dyna_config_swimmer_test.yml'
     
+    # TO-DO: replace with argparse
+    filename = os.path.join(_parent_dir, 
+                            'config_templates', 
+                            'dyna_config_cart.yml' # replace with appropriate config yaml
+                            )
+    
+    # load config
     with open(filename, 'r') as f:
         dyna_config = yaml.load(f, Loader=yaml.SafeLoader)
     LOCAL_DIR =  os.path.join(_parent_dir, 'ray_results',dyna_config['exp_dir'])
-    
     pprint(dyna_config)
     
     # Setup logger
@@ -169,18 +128,18 @@ if __name__ == '__main__':
     logger = logging.getLogger('dyna-sindy')
     logger.setLevel(logging.INFO)
 
-    # TO-DO: find a better place to automate this
-    # n_control = dyna_config['dynamics_model']['config']['feature_library']['kwargs']['n_control']
+    # Initialize default off-policy for initial collection
     n_control = dyna_config['drl']['config']['environment']['env_config']['act_dim']
     dyna_config['off_policy_pi'] = RandomPolicy(low=-1*np.ones(n_control), 
                                                 high = np.ones(n_control), 
                                                 seed=0)
     
+    # setup ray
     ip_head = os.environ.get('ip_head', None)
     ray.init(address=ip_head, 
              logging_level=logging.ERROR)
-    print(ray.nodes())
 
+    # PBT-specific config
     pbt_sched = None
     if dyna_config.get('use_pbt', False):
         pbt_config = dyna_config['pbt_config']
@@ -196,16 +155,12 @@ if __name__ == '__main__':
                         custom_explore_fn=explore
                     )
 
+    # ray + tune configs
     ray_config = dyna_config['ray_config']
     run_config=air.RunConfig(
         local_dir=LOCAL_DIR,
-        # storage_path=LOCAL_DIR,
-        # checkpoint_config=air.CheckpointConfig(
-        #     **ray_config.get('checkpoint_config', {})
-        # ),
         **ray_config['run_config']
     )
-    
     
     tune_config=tune.TuneConfig(
                     **dyna_config['ray_config']['tune_config'],
