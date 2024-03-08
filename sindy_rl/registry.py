f804abd59ab8e379b1dbab54411ea821fbb96069
Nick Zolman <nzolman@uw.edu>
2024-03-08 11:32:47 -0800

    registry comments

--- sindy_rl/registry.py
+++ sindy_rl/registry.py
@@ -1,11 +1,15 @@
+# intention to provide a convenient place to load custom/wrapped environments from
+# when loading classes/configs from strings.
+
 import warnings
 import numpy as np
 from gymnasium.envs.mujoco.swimmer_v4 import SwimmerEnv
 from ray.rllib.env.wrappers.dm_control_wrapper import DMCEnv
 
-from sindy_rl.refactor.swimmer import SwimmerWithBounds, SwimmerWithBoundsClassic
+from sindy_rl.swimmer import SwimmerWithBounds, SwimmerWithBoundsClassic
 from sindy_rl.reward_fns import cart_reward
 
+# Don't require that user needs hydrogym installed
 try: 
     from sindy_rl.hydroenv import CylinderLiftEnv, PinballLiftEnv, CylinderWrapper
 except ImportError:
@@ -23,6 +27,7 @@ class DMCEnvWrapper(DMCEnv):
         super().__init__(**env_config)
 
 class SwimmerWrapper(SwimmerEnv):
+    '''Wrapper to ensure compliant with RLib's config requirements'''
     def __init__(self, config=None):
         env_config = config or {}
         super().__init__(**env_config)
